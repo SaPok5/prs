66a5ac569127f723d9e6dbaeefe6dd869d4662c0
SaPok5 <samippokhrel5@gmail.com>
2025-05-26 12:13:07 +0545

    lol4

--- backend/tests/example.test.ts
+++
@@ -1,9 +0,0 @@
-describe('Example Test Suite', () => {
-  it('should pass this simple test', () => {
-    expect(true).toBe(true);
-  });
-
-  it('should perform basic addition', () => {
-    expect(1 + 1).toBe(2);
-  });
-});
