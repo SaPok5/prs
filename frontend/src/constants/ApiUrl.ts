2df46fbd78f659c82297ae5b681f7b5feb505659
SaPok5 <samippokhrel5@gmail.com>
2025-05-26 15:56:18 +0545

    frontend

--- frontend/src/constants/ApiUrl.ts
+++ frontend/src/constants/ApiUrl.ts
@@ -1,3 +1,3 @@
-// export const ServerURL = "https://apiprs.brahmabytelab.com"
-export const ServerURL = "http://localhost:8000"
+export const ServerURL = "https://prs-backend-z45h.onrender.com/"
+// export const ServerURL = "http://localhost:8000"
 export const BaseApiURL = `${ServerURL}/api/v1`;
